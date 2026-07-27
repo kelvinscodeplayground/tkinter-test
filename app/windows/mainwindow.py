b48829d9d2ff20728bd728ac804985d3d8a91677
Kelvin Chin <chinkaiwen_35@outlook.com>
2026-07-27 21:30:54 +0800

    Refactor MainWindow initialization and setup
    Update MainWindow to inherit from tk.Tk and simplify initialization

--- app/windows/mainwindow.py
+++ app/windows/mainwindow.py
@@ -4,23 +4,20 @@ from tkinter import ttk
 from app.utils import file_system as fs
 
 
-class MainWindow:
+class MainWindow(tk.Tk):
     __WINDOW_SIZE = (640, 480)
     __APP_ICON_PATH = fs.create_path("/assets/icons/tools.png")
 
-    __window: tk.Tk
     __label: ttk.Label
     __button: ttk.Button
 
     def __init__(self):
-        self.__window = tk.Tk()
-        self.__window.title("My Tkinter App")
-        self.__window.geometry(f"{self.__WINDOW_SIZE[0]}x{self.__WINDOW_SIZE[1]}")
+        super().__init__()
+
+        self.title("My Tkinter App")
+        self.geometry(f"{self.__WINDOW_SIZE[0]}x{self.__WINDOW_SIZE[1]}")
 
         self.__setup()
-
-    def mainloop(self):
-        self.__window.mainloop()
 
     # Private methods
     def __setup(self):
@@ -30,24 +27,24 @@ class MainWindow:
 
     def __initialize_ui(self):
         # initialize lablel, then set it to aligned left
-        self.__label = ttk.Label(self.__window, text="Hello, Tkinter!")
+        self.__label = ttk.Label(self, text="Hello, Tkinter!")
         self.__label.pack(side="top")
 
         self.__button = ttk.Button(
-            self.__window, text="Click Me!", command=self.__on_button_click
+            self, text="Click Me!", command=self.__on_button_click
         )
         self.__button.pack(side="top")
 
     def __center_window(self):
-        screen_width = self.__window.winfo_screenwidth()
-        screen_height = self.__window.winfo_screenheight()
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
         x = (screen_width // 2) - (self.__WINDOW_SIZE[0] // 2)
         y = (screen_height // 2) - (self.__WINDOW_SIZE[1] // 2)
-        self.__window.geometry(f"+{x}+{y}")
+        self.geometry(f"+{x}+{y}")
 
     def __set_app_icon(self):
         bitmap = tk.PhotoImage(file=fs.get_abs_file_path(self.__APP_ICON_PATH))
-        self.__window.wm_iconphoto(False, bitmap)
+        self.wm_iconphoto(False, bitmap)
 
     # Event handlers
     def __on_button_click(self):
