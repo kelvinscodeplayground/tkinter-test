f7be7fdded07f37e9fa9deaaca331a0b58fc2348
Kelvin Chin <chinkaiwen_35@outlook.com>
2026-07-27 10:04:08 +0800

    Update tasks.json and mainwindow for configuration changes

--- app/windows/mainwindow.py
+++ app/windows/mainwindow.py
@@ -4,8 +4,8 @@ from app.utils import file_system as fs
 
 
 class MainWindow:
-    __window_size = (640, 480)
-    __app_icon_path = fs.create_path("/assets/icons/tools.png")
+    __WINDOW_SIZE = (640, 480)
+    __APP_ICON_PATH = fs.create_path("/assets/icons/tools.png")
 
     __window: tk.Tk
     __label: tk.Label
@@ -13,7 +13,7 @@ class MainWindow:
     def __init__(self):
         self.__window = tk.Tk()
         self.__window.title("My Tkinter App")
-        self.__window.geometry(f"{self.__window_size[0]}x{self.__window_size[1]}")
+        self.__window.geometry(f"{self.__WINDOW_SIZE[0]}x{self.__WINDOW_SIZE[1]}")
 
         self.__setup()
 
@@ -36,13 +36,10 @@ class MainWindow:
     def __center_window(self):
         screen_width = self.__window.winfo_screenwidth()
         screen_height = self.__window.winfo_screenheight()
-        x = (screen_width // 2) - (self.__window_size[0] // 2)
-        y = (screen_height // 2) - (self.__window_size[1] // 2)
+        x = (screen_width // 2) - (self.__WINDOW_SIZE[0] // 2)
+        y = (screen_height // 2) - (self.__WINDOW_SIZE[1] // 2)
         self.__window.geometry(f"+{x}+{y}")
 
     def __set_app_icon(self):
-        try:
-            bitmap = tk.PhotoImage(file=fs.get_abs_file_path(self.__app_icon_path))
-            self.__window.wm_iconphoto(False, bitmap)
-        except Exception as e:
-            print(f"Error setting app icon: {e}")
+        bitmap = tk.PhotoImage(file=fs.get_abs_file_path(self.__APP_ICON_PATH))
+        self.__window.wm_iconphoto(False, bitmap)
